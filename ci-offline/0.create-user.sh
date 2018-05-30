da57daa32b77be545abc365531107226456e8748
root <you@example.com>
2018-05-30 21:08:50 +0900

    offline script

---
+++ ci-offline/0.create-user.sh
@@ -0,0 +1,3 @@
+# fly -t totutial set-team -n ci-offline --basic-auth-username <username> --basic-auth-password <password>
+fly -t tutorial set-team -n ci-offline  --basic-auth-username useroffline1 --basic-auth-password password1  
+
--- ci-offline/1.login.sh
+++ ci-offline/1.login.sh
@@ -1,3 +1,1 @@
-# fly -t totutial set-team -n ci-offline --basic-auth-username <username> --basic-auth-password <password>
-../fly -t tutorial login -c http://127.0.0.1:8080 -n ci-offline 
-
+fly -t tutorial login -c http://127.0.0.1:8080 -n ci-offline --username=useroffline1 --password=password1
